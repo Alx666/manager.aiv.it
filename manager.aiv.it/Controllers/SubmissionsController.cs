ac0f806ac934a0d74fe34b4e6f51b2bcdfb92e22
alxeyesoul <alxeyesoul@live.com>
2016-10-07 16:57:35 +0200

    Submit Upload

---
+++ manager.aiv.it/Controllers/SubmissionsController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using manager.aiv.it;
+
+namespace manager.aiv.it.Controllers
+{
+    public class SubmissionsController : Controller
+    {
+        private AivEntities db = new AivEntities();
+
+        // GET: Submissions
+        public ActionResult Index()
+        {
+            var submissions = db.Submissions.Include(s => s.Student).Include(s => s.Revisor).Include(s => s.Assignment).Include(s => s.Binary);
+            return View(submissions.ToList());
+        }
+
+        // GET: Submissions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Submission submission = db.Submissions.Find(id);
+            if (submission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(submission);
+        }
+
+        // GET: Submissions/Create
+        public ActionResult Create()
+        {
+            ViewBag.StudentId = new SelectList(db.Users, "Id", "Name");
+            ViewBag.RevisorId = new SelectList(db.Users, "Id", "Name");
+            ViewBag.AssignmentId = new SelectList(db.Assignments, "Id", "Description");
+            ViewBag.BinaryId = new SelectList(db.Binaries, "Id", "Filename");
+            return View();
+        }
+
+        // POST: Submissions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult Upload(int AssignmentId, HttpPostedFileBase upload)
+        {
+            User hUser = db.Users.Find((int)this.Session["UserId"]);
+            if(hUser == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            if (ModelState.IsValid)
+            {
+                //db.Submissions.Add(submission);
+                //db.SaveChanges();
+            }
+
+            return View();
+        }
+
+        // GET: Submissions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Submission submission = db.Submissions.Find(id);
+            if (submission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(submission);
+        }
+
+        // POST: Submissions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Submission submission = db.Submissions.Find(id);
+            db.Submissions.Remove(submission);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
