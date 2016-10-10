11a836cfc95257bbff46ee324d985eaa961eeb72
redsanso <redsanso@gmail.com>
2016-10-10 23:55:14 +0200

    - minor tuning

--- manager.aiv.it/Models/Submission.cs
+++ manager.aiv.it/Models/Submission.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ namespace manager.aiv.it
     [MetadataType(typeof(ISubmissionMetaData))]
     public partial class Submission
     {
+        [DisplayName("Name")]
+        public string DisplayName => $"Submission";
     }
 
     public interface ISubmissionMetaData
