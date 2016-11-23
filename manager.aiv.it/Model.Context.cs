9ded3bf3b4f8022e064c51102baf3fd7d63984ff
alxeyesoul <alxeyesoul@live.com>
2016-11-23 18:15:48 +0100

    Model update, implementate view, corretto problema performance

--- manager.aiv.it/Model.Context.cs
+++ manager.aiv.it/Model.Context.cs
@@ -38,5 +38,7 @@ namespace manager.aiv.it
         public virtual DbSet<Submission> Submissions { get; set; }
         public virtual DbSet<Topic> Topics { get; set; }
         public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<ViewLessonFullData> ViewLessonFullDatas { get; set; }
+        public virtual DbSet<ViewLessonPresence> ViewLessonPresences { get; set; }
     }
 }
